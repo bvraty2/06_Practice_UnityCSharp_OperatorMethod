b1c3f97560848c52b40c6beeeabca494df225d7b
bvraty2 <63327363+bvraty2@users.noreply.github.com>
2020-05-05 14:05:12 +0800

    workdone

--- Assets/Bat.cs
+++ Assets/Bat.cs
@@ -1,17 +1,46 @@
 ﻿
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Bat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("史萊姆")]
+    public Slime slime;
+    
+    private float hp = 150;
+    private float atk = 50;
+    private float cure = 20;
+
+
+    public void Attack()
     {
-        
+        slime.Damage(atk);
+    }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        message.text = gameObject.name + " - 受到傷害: " + damage;
+        message.text += "\n" + gameObject.name + " - 血量剩下: " + hp;
+     }
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        hp += cure;
+        message.text = gameObject.name + " - 受到治癒: " + cure;
+        message.text += "\n" + gameObject.name + " - 血量剩下: " + hp;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+
+
+
+
+
---
+++ Assets/Slime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Slime : MonoBehaviour
+{
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("蝙蝠")]
+    public Bat bat;
+
+    private float hp = 200;
+    private float atk = 20;
+    private float cure = 10;
+
+
+    public void Attack()
+    {
+        bat.Damage(atk);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        message.text = gameObject.name + " - 受到傷害: " + damage;
+        message.text += "\n" + gameObject.name + " - 血量剩下: " + hp;
+    }
+    public void Cure()
+    {
+        hp -= cure;
+        message.text = gameObject.name + " - 受到治癒: " + cure;
+        message.text += "\n" + gameObject.name + " - 血量剩下: " + hp;
+    }
+}
